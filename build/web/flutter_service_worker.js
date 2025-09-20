b5edce0e38d5291cb0c9bb9b96dc06b94a367c95
Adryc Allen Catapang <adrycallencatapang@Adrycs-MacBook-Air.local>
2025-09-20 20:35:13 +0800

    fix: icon; title

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "c587a8bbc9e8235846aa2cacfa45247f",
-"version.json": "c754f166a63b20d20666dd883ebc90d4",
+const RESOURCES = {"flutter_bootstrap.js": "00f03ca057bcab62cba472bb44798724",
+"version.json": "2ff852a92f7bb1fc8e3a8ccd17a54bf8",
 "splash/img/light-2x.png": "8dda9abb5763ffd253ba19b6d78899df",
 "splash/img/dark-4x.png": "652d18c69174c263a1a0f6225d588ae4",
 "splash/img/light-3x.png": "ca6e4dfdba5e5a26be7e4b7d604bd017",
@@ -13,9 +13,9 @@ const RESOURCES = {"flutter_bootstrap.js": "c587a8bbc9e8235846aa2cacfa45247f",
 "splash/img/dark-2x.png": "8dda9abb5763ffd253ba19b6d78899df",
 "splash/img/dark-1x.png": "a6dd5cf632ebb0d1ee62a5d8e453f684",
 "splash/img/light-1x.png": "a6dd5cf632ebb0d1ee62a5d8e453f684",
-"index.html": "3183775e540b55ccf07e1bbfb70a6fe9",
-"/": "3183775e540b55ccf07e1bbfb70a6fe9",
-"main.dart.js": "6cb94e8325865c5cb1122d1d2a101ee5",
+"index.html": "1257218767a4fc04a6dcd71769c6ade8",
+"/": "1257218767a4fc04a6dcd71769c6ade8",
+"main.dart.js": "13eadd340e5c3fad7efc84c1ed568e15",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
 "favicon.png": "3ae20e30d41074c0cc29e086025631ca",
 "icons/webicon.png": "33b5b946976d09ebfd46c6644a1bf601",
