79e96bc4a4e140cc6f97d1b461fe38a12e70c101
Adryc Allen Catapang <catapangadrycallen@gmail.com>
2026-01-22 16:37:06 +0800

    hotfix: stripe

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "6ffc74e7f5658d3b3877fdca382d6f6c",
+const RESOURCES = {"flutter_bootstrap.js": "db90bcf4e9e6f465d46d7433d6e5017f",
 "version.json": "323ddc6bb293573968194d9c20590d1b",
 "splash/img/light-2x.png": "8dda9abb5763ffd253ba19b6d78899df",
 "splash/img/dark-4x.png": "652d18c69174c263a1a0f6225d588ae4",
@@ -13,8 +13,8 @@ const RESOURCES = {"flutter_bootstrap.js": "6ffc74e7f5658d3b3877fdca382d6f6c",
 "splash/img/dark-2x.png": "8dda9abb5763ffd253ba19b6d78899df",
 "splash/img/dark-1x.png": "a6dd5cf632ebb0d1ee62a5d8e453f684",
 "splash/img/light-1x.png": "a6dd5cf632ebb0d1ee62a5d8e453f684",
-"index.html": "e84ae97c16d7011e7a22e92cf52d01e6",
-"/": "e84ae97c16d7011e7a22e92cf52d01e6",
+"index.html": "360ef911cacb2d90692af4ca66359671",
+"/": "360ef911cacb2d90692af4ca66359671",
 "firebase-messaging-sw.js": "c9a485aedca68a0491edda5d0cfbb5d7",
 "main.dart.js": "a9fb055ee12311cef21365518ccc86fa",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
