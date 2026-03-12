bcd5ec4b430729e6f7305637899d87147f35b97e
Adryc Allen Catapang <catapangadrycallen@gmail.com>
2026-03-12 22:23:57 +0800

    revert: install pwa

--- lib/view_models/splash.vm.dart
+++ lib/view_models/splash.vm.dart
@@ -41,8 +41,8 @@ class SplashViewModel extends BaseViewModel {
     await AuthService.getUserFromStorage();
     await AuthService.getTokenFromStorage();
     try {
-      version = "1.0.30";
-      versionCode = "50";
+      version = "1.0.50";
+      versionCode = "70";
     } catch (e) {
       debugPrint(
         "getAppInfo error: $e",
